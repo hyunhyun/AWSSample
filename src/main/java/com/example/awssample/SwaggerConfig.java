41c88dbb907bfa5304fe7981ea5b7c4e00fda8bc
hyunhyun <jhyun_yi@naver.com>
2023-06-15 10:23:49 +0900

    spring boot swagger 설정

---
+++ src/main/java/com/example/awssample/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.awssample;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    Info info = new Info().version("v1.0.0").title("제목").description("설명");
+
+    return new OpenAPI().info(info);
+  }
+
+
+}
